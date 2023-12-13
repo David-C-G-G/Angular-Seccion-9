f1116f370eb0e2fc454afe6066700b6e4f65a509
David Gutiérrez <lodevil25@gmail.com>
2023-12-12 23:38:30 -0600

    fin sección 9

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'countries-country-page',
@@ -10,6 +11,8 @@ import { switchMap } from 'rxjs';
   ]
 })
 export class CountryPageComponent implements OnInit{
+
+  public country?: Country;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -23,14 +26,8 @@ export class CountryPageComponent implements OnInit{
       switchMap( ({id}) => this.countriesService.searchCountryByAlphaCode( id ) )
     )
     .subscribe( country => {
-
-      if(!country){
-        return this.router.navigateByUrl('');
-      }
-
-      console.log('Tenemos un País');
-      return;
-
+      if(!country)return this.router.navigateByUrl('');
+      return this.country = country;
     });
   }
 
